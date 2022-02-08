d34259db90733b7eadc866b9a85ce69239d5a0b4
Leonardo <albornozleo95@gmail.com>
2022-02-08 18:55:55 -0400

    Fix linters

--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,19 @@
 import propTypes from 'prop-types';
+import React from 'react';
 
-export default function Button(props) {
-  let { operator } = props;
-  const { children } = props;
-  operator = operator
-    ? `calculator_button calculator_button_${operator}`
-    : 'calculator_button';
-  return (
-    <button type="button" className={operator} aria-label="calculator_btn">
-      {children}
-    </button>
-  );
+export default class Button extends React.Component {
+  render() {
+    let { operator } = this.props;
+    const { children } = this.props;
+    operator = operator
+      ? `calculator_button calculator_button_${operator}`
+      : 'calculator_button';
+    return (
+      <button type="button" className={operator} aria-label="calculator_btn">
+        {children}
+      </button>
+    );
+  }
 }
 
 Button.propTypes = {
